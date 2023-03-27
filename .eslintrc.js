8bf27a7ca288d84b88a4e6392af33b49d149e95d
Josh Curtiss <josh@curtiss.me>
2023-03-27 16:29:01 -0500

    feat: Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+   root: true,
+   env: {
+      node: true,
+   },
+   extends: [
+      '@vue/typescript/recommended',
+      'plugin:vue/vue3-recommended',
+      '@vue/airbnb',
+   ],
+   parserOptions: {
+      ecmaVersion: 2020,
+   },
+   settings: {
+      'import/resolver': {
+         node: {
+            extensions: [ '.js', '.jsx', '.ts', '.tsx' ],
+         },
+      },
+   },
+   rules: {
+      'array-bracket-spacing': [ 'error', 'always' ],
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-underscore-dangle': [ 'error', { allowAfterThis: true } ],
+      indent: [ 'error', 3 ],
+      'import/extensions': [
+         'error',
+         'ignorePackages',
+         {
+            js: 'never',
+            jsx: 'never',
+            ts: 'never',
+            tsx: 'never',
+         },
+      ],
+      'import/no-extraneous-dependencies': [ 'error', { devDependencies: [ '**/*.test.{j,t}s', 'vue.config.{j,t}s' ] } ],
+      'lines-between-class-members': [ 'error', 'always', { exceptAfterSingleLine: true } ],
+      '@typescript-eslint/no-var-requires': 'off',
+   },
+   overrides: [
+      {
+         files: [ '**/*.vue' ],
+         parser: 'vue-eslint-parser',
+         env: {
+            'vue/setup-compiler-macros': true,
+            browser: true,
+         },
+         rules: {
+            'vue/html-indent': [ 'error', 3 ],
+         },
+      },
+      {
+         files: [ '**/*.test.ts' ],
+         env: {
+            jest: true,
+         },
+      },
+   ],
+};
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const VALID_TYPES = [
    'build',
    'chore',
